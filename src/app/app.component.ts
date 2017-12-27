39a4a3fdbac932fc61ad5c0392b7b9291f82a1ac
surajsinghmehta <suraj.mehta@ongraph.com>
2017-12-27 14:44:50 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit {
+  public loginForm: FormGroup;
+  error: any;
+  constructor(private formBuilder: FormBuilder) { }
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+  login(obj: any) {
+    if (this.loginForm.valid) {
+      console.log(obj);
+    }else {
+      console.log('error');
+      this.error = true;
+    }
+  }
 }
