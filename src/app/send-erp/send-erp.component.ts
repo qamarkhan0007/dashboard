5da9a5d9dfbef977bd67136ae6a8dc176e61e641
surajsinghmehta <suraj.mehta@ongraph.com>
2018-01-05 12:51:09 +0530

    UI

--- src/app/send-erp/send-erp.component.ts
+++ src/app/send-erp/send-erp.component.ts
@@ -11,21 +11,26 @@ import { Router } from '@angular/router';
 export class SendErpComponent implements OnInit {
     brand: any;
     erpData: any;
-    distat: any ;
+    distat: any = false;
   constructor(private route: ActivatedRoute, private _service: AppService) { }
 
   ngOnInit() {
-      this.distat = false;
       this.route.params.subscribe((params: Params) => {
           this.brand = params['brand'];
   });
   }
   ErpSend(select, perform, payload) {
+      let that: any;
       this._service.ErpSend(select.value, perform.value, payload.value, this.brand).subscribe(res => {
           this.erpData = res.data;
-          this.distat = 'true';
+          this.distat = true;
       });
       payload.value = '';
       perform.value = '';
+      that = this;
+      setTimeout(function () {
+        that.distat = false;
+      }, 1000);
+      return false;
   }
   }
